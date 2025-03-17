8131eabe85d5df4fa8d2ec88ee4f974fad164f0d
ayoub-ben-99 <user.name>
2025-03-17 22:54:33 +0000

    edit file

--- src/app/home/page.jsx
+++ src/app/home/page.jsx
@@ -1,14 +1,14 @@
 import Navbar from '../_components/navbar';
 import Footer from '../_components/footer';
-import Hero from './hero';
-import PecialOffers from './pecialOffers';
+import Hero from '../_components/hero';
+import PecialOffers from '../_components/pecialOffers';
 
 export default function Home() {
     return (
         <div className="overflow-x-hidden w-screen h-full">
             <Navbar />
             <Hero />
-            <PecialOffers/>
+            <PecialOffers />
             <Footer />
         </div>
     );
